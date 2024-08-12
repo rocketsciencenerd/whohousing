e6662e952dc665ad74b5326876049cffb7481c7c
Katie Heinemann <katie@violetlabs.com>
2024-08-12 09:45:01 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  base: '/',
+  base: '/whohousing/',
   plugins: [
     vue(),
     vueJsx(),
